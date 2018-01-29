46a166de45e067522a5c5cc13bff6c7b93919192
Alan English <aenglish@manorhillcapital.com>
2018-01-29 13:23:46 -0600

    remove all code from app.js file and create AppRouter component - import into app.js and render to DOM

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my Dashboard Component.
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my AddExpense Component.
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from my EditExpense Component.
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my Help Component.
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404 Not Found! - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+);
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route
+          path="/"
+          component={ExpenseDashboardPage}
+          exact={true}
+        />
+        <Route
+          path="/create"
+          component={AddExpensePage}
+        />
+        <Route
+          path="/edit"
+          component={EditExpensePage}
+        />
+        <Route
+          path="/help"
+          component={HelpPage}
+        />
+        <Route
+          component={NotFoundPage}
+        />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
