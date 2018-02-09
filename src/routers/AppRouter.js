81c377234962505553c16a1b48ec30b136ed3582
Alan English <aenglish@manorhillcapital.com>
2018-02-09 16:54:14 -0600

    refactor edit route to include an id, remove edit expense component from rendering on the header component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -22,7 +22,7 @@ const AppRouter = () => (
           component={AddExpensePage}
         />
         <Route
-          path="/edit"
+          path="/edit/:id"
           component={EditExpensePage}
         />
         <Route
