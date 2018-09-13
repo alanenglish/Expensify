90fc2127413748c4e1a046707cbeb000b122d37a
Alan English <aenglish@manorhillcapital.com>
2018-09-13 18:22:13 -0500

    add react redux, create ExpenseList, ExpenseListItem, and ExpenseListFilters component

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
@@ -10,12 +11,18 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill' }));
-store.dispatch(addExpense({ description: 'Gas Bill' }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const App = (
+  <Provider store={store} >
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(App, document.getElementById('app'));
