abff9e358cb9cb8d49a6c94e8bbe180c0dfd2f8c
Alan English <aenglish@manorhillcapital.com>
2018-09-15 17:21:13 -0500

    wire up edit expense page using reusable component, create ability to filter by dates using DateRangePicker from react dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,14 +4,18 @@ import { SingleDatePicker } from 'react-dates'; // requries moment
 import 'react-dates/lib/css/_datepicker.css'; // css for react dates calendar
 
 class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
-    calendarFocused: false,
-    error: ''
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount /100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) :moment(),
+      calendarFocused: false,
+      error: ''
+    };
+  }
 
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -96,7 +100,7 @@ class ExpenseForm extends React.Component {
             value={this.state.note}
             onChange={this.onNoteChange}
           />
-          <button>Add Expense</button>
+          <button>{this.props.expense ? 'Update Expense' : 'Add Expense'}</button>
         </form>
       </div>
     );
