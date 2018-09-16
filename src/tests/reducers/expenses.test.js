c5d4e9189414ce1d90dda53592f06ec9c339189d
Alan English <aenglish@manorhillcapital.com>
2018-09-15 23:54:59 -0500

    create tests for all actions, reducers and selectors - all tests passing

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,82 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+/* eslint-env jest */
+
+test('should set the default state', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = { type: 'REMOVE_EXPENSE', id: expenses[1].id };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expense if id is not found', () => {
+  const action = { type: 'REMOVE_EXPENSE', id: '-1' };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test('should addExpense', () => {
+  const expense = {
+    id: '4',
+    description: 'Peoples Gas',
+    note: '',
+    amount: 8900,
+    createdAt: 0
+  };
+
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense
+  };
+
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, expense]);
+});
+
+test('should editExpense', () => {
+  const edits = {
+    description: 'Misc',
+    note: 'Walgreens Purchase',
+    amount: 1500
+  };
+
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[0].id,
+    updates: {
+      ...edits
+    }
+  };
+
+  const state = expensesReducer(expenses, action);
+  expect(state[0]).toEqual({
+    id: '1',
+    description: 'Misc',
+    note: 'Walgreens Purchase',
+    amount: 1500,
+    createdAt: 0
+  });
+});
+
+test('should not editExpense if expense not found', () => {
+  const edits = {
+    description: 'Misc',
+    note: 'Walgreens Purchase',
+    amount: 1500
+  };
+
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: {
+      ...edits
+    }
+  };
+
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
