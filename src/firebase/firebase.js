5cb6feaab3c29bf616f8126589f3c024ecbdc64e
Alan English <aenglish@manorhillcapital.com>
2018-09-29 20:45:20 -0400

    setup test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBz1IOM1VahrTsD94783hIz3aDCKSwcmBk',
-  authDomain: 'expensify-13d78.firebaseapp.com',
-  databaseURL: 'https://expensify-13d78.firebaseio.com',
-  projectId: 'expensify-13d78',
-  storageBucket: 'expensify-13d78.appspot.com',
-  messagingSenderId: '165463597143'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
